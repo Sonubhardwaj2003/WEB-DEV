7d70d45382d99b07993cb3b155dfcefee70c182e
Sonubhardwaj2003 <sb9432024@gmail.com>
2025-12-06 20:46:24 +0530

    todolist

--- harsh.js
+++
@@ -1,9 +0,0 @@
-let div=document.querySelector("div");
-
-div.addEventListener("mouseover",function(){
-    div.style.backgroundColor="yellow";
-})
-
-div.addEventListener("mouseout",function(){
-    div.style.backgroundColor="red";
-})
