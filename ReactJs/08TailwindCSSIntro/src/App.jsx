5bdee22594846ac20c5ad85f8f5a97e4bb6ed4f2
Sonubhardwaj2003 <sb9432024@gmail.com>
2026-01-31 17:05:23 +0530

    tailwindPratice

---
+++ ReactJs/08TailwindCSSIntro/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='h-screen bg-gray-600 m-0'>Div</div>
+    </>
+  )
+}
+
+export default App
